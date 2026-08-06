aadb7f4956831effd41754e05aff1baa23bfedf9
kohnish <kohnish@gmx.com>
2026-08-06 22:00:51 +0200

    WIP: start caching for git status before replacing

--- lisp/magit-mode.el
+++ lisp/magit-mode.el
@@ -53,14 +53,21 @@
 
 ;;; Options
 
-(defcustom magit-mode-hook
-  (list #'magit-load-config-extensions)
+;; (defcustom magit-mode-hook
+;;   (list #'magit-load-config-extensions)
+;;   "Hook run when entering a mode derived from Magit mode."
+;;   :package-version '(magit . "3.0.0")
+;;   :group 'magit-modes
+;;   :type 'hook
+;;   :options (list #'magit-load-config-extensions
+;;                  #'bug-reference-mode))
+;; WIP: deprecate
+(defcustom magit-mode-hook nil
   "Hook run when entering a mode derived from Magit mode."
   :package-version '(magit . "3.0.0")
   :group 'magit-modes
   :type 'hook
-  :options (list #'magit-load-config-extensions
-                 #'bug-reference-mode))
+  :options nil)
 
 (defcustom magit-setup-buffer-hook
   (list #'magit-maybe-save-repository-buffers
@@ -76,6 +83,22 @@ should be used instead of this one."
   :group 'magit-modes
   :type 'hook
   :options (list #'magit-maybe-save-repository-buffers
+                 'magit-set-buffer-margin))
+
+(defcustom magit-setup-buffer-hook-new
+  (list #'magit-maybe-save-repository-buffers-new
+        'magit-set-buffer-margin) ; from magit-margin.el
+  "Hook run by `magit-setup-buffer'.
+
+This is run right after displaying the buffer and right before
+generating or updating its content.  `magit-mode-hook' and other,
+more specific, `magit-mode-*-hook's on the other hand are run
+right before displaying the buffer.  Usually one of these hooks
+should be used instead of this one."
+  :package-version '(magit . "2.3.0")
+  :group 'magit-modes
+  :type 'hook
+  :options (list #'magit-maybe-save-repository-buffers-new
                  'magit-set-buffer-margin))
 
 (defcustom magit-pre-refresh-hook
@@ -633,6 +656,14 @@ The buffer's major-mode should derive from `magit-section-mode'."
                            `(list ',var ,form))
                          bindings))))
 
+(defmacro magit-setup-buffer-new (git-info mode &optional locked &rest bindings)
+  (declare (indent 2))
+  `(magit-setup-buffer-internal-new
+    ,git-info ,mode ,locked
+    ,(cons 'list (mapcar (pcase-lambda (`(,var ,form))
+                           `(list ',var ,form))
+                         bindings))))
+
 (defun magit-setup-buffer-internal ( mode locked bindings
                                      &optional buffer-or-name directory)
   (let* ((value   (and locked
@@ -664,6 +695,43 @@ The buffer's major-mode should derive from `magit-section-mode'."
       (magit-refresh-buffer)
       (when created
         (run-hooks 'magit-post-create-buffer-hook)))
+    buffer))
+
+(defvar git-info-for-hooks nil)
+
+(defun magit-setup-buffer-internal-new ( git-info mode locked bindings
+                                         &optional buffer-or-name directory)
+  (setq git-info-for-hooks git-info)
+  (let* ((value   (and locked
+                       (with-temp-buffer
+                         (pcase-dolist (`(,var ,val) bindings)
+                           (set (make-local-variable var) val))
+                         (let ((major-mode mode))
+                           (magit-buffer-value)))))
+         (buffer  (if buffer-or-name
+                      (get-buffer-create buffer-or-name)
+                    (magit-get-mode-buffer-new (magit-git-info-get 'git-root git-info) mode value)))
+         (section (and buffer (magit-current-section)))
+         (created (not buffer)))
+    (unless buffer
+      (setq buffer (magit-generate-new-buffer mode value (magit-git-info-get 'git-root git-info))))
+    (with-current-buffer buffer
+      (setq magit-previous-section section)
+      (when directory
+        (setq default-directory directory))
+      (funcall mode)
+      (magit-xref-setup #'magit-setup-buffer-internal bindings)
+      (pcase-dolist (`(,var ,val) bindings)
+        (set (make-local-variable var) val))
+      (when created
+        (run-hooks 'magit-create-buffer-hook)))
+    (magit-display-buffer buffer)
+    (with-current-buffer buffer
+      (run-hooks 'magit-setup-buffer-hook)
+      (magit-refresh-buffer)
+      (when created
+        (run-hooks 'magit-post-create-buffer-hook)))
+    (setq git-info-for-hooks nil)
     buffer))
 
 ;;; Display Buffer
@@ -879,6 +947,43 @@ If a frame, then only consider buffers on that frame."
         ('visible               (seq-some #'f (visible-frame-list)))
         ((or 'selected 't)      (seq-some #'w (window-list (selected-frame))))
         ((guard (framep frame)) (seq-some #'w (window-list frame)))))))
+
+(defun magit-get-mode-buffer-new (topdir mode &optional value frame)
+  "Return buffer belonging to the current repository whose major-mode is MODE.
+
+If no such buffer exists then return nil.  Multiple buffers with
+the same major-mode may exist for a repository but only one can
+exist that hasn't been locked to its value.  Return that buffer
+\(or nil if there is no such buffer) unless VALUE is non-nil, in
+which case return the buffer that has been locked to that value.
+
+If FRAME is nil or omitted, then consider all buffers.  Otherwise
+  only consider buffers that are displayed in some live window
+  on some frame.
+If `all', then consider all buffers on all frames.
+If `visible', then only consider buffers on all visible frames.
+If `selected' or t, then only consider buffers on the selected
+  frame.
+If a frame, then only consider buffers on that frame."
+  (cl-flet* ((b (buffer)
+               (with-current-buffer buffer
+                 (and (eq major-mode mode)
+                      (equal magit--default-directory topdir)
+                      (if value
+                          (and magit-buffer-locked-p
+                               (equal (magit-buffer-value) value))
+                        (not magit-buffer-locked-p))
+                      buffer)))
+             (w (window)
+               (b (window-buffer window)))
+             (f (frame)
+               (seq-some #'w (window-list frame 'no-minibuf))))
+    (pcase-exhaustive frame
+      ('nil                   (seq-some #'b (buffer-list)))
+      ('all                   (seq-some #'f (frame-list)))
+      ('visible               (seq-some #'f (visible-frame-list)))
+      ((or 'selected 't)      (seq-some #'w (window-list (selected-frame))))
+      ((guard (framep frame)) (seq-some #'w (window-list frame))))))
 
 (defun magit-generate-new-buffer (mode &optional value directory)
   (let* ((default-directory (or directory (magit--toplevel-safe)))
@@ -1185,6 +1290,11 @@ if you so desire."
 
 (defvar-local magit-inhibit-refresh-save nil)
 
+(defun magit-top-level (git-info)
+  (if-let ((root (magit-git-info-get 'git-root git-info)))
+    root
+    (magit-rev-parse-safe "--show-toplevel")))
+
 (defun magit-save-repository-buffers (&optional arg)
   "Save file-visiting buffers belonging to the current repository.
 After any buffer where `buffer-save-without-query' is non-nil
@@ -1192,7 +1302,7 @@ is saved without asking, the user is asked about each modified
 buffer which visits a file in the current repository.  Optional
 argument (the prefix) non-nil means save all with no questions."
   (interactive "P")
-  (when-let ((topdir (magit-rev-parse-safe "--show-toplevel")))
+  (when-let ((topdir (magit-top-level git-info-for-hooks)))
     (let ((remote (file-remote-p default-directory))
           (save-some-buffers-action-alist
            `((?Y (lambda (buffer)
@@ -1233,7 +1343,7 @@ argument (the prefix) non-nil means save all with no questions."
                 ;;
                 ;; Check whether the file is inside the repository.
                 (equal (or (cdr (assoc default-directory topdirs))
-                           (let ((top (magit-rev-parse-safe "--show-toplevel")))
+                           (let ((top (magit-top-level git-info-for-hooks)))
                              (push (cons default-directory top) topdirs)
                              top))
                        topdir)
